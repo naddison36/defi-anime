718546ad565a1561fb30f3ff3e6f6c38049bff11
Nicholas Addison <nick@addisonbrown.com.au>
2023-04-15 09:28:47 +1000

    add timer to cycle through flows

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,11 @@
 import '../style.css';
 import { parsePoolBalances } from './parsers';
 import { render } from './reserveProduct';
+import { Flow } from './typings';
 import { getFlows, getReserves } from './uniswap';
+
+// Time in milliseconds between each flow
+export const flowDuration = 500;
 
 // TODO hard code for now. Ideally these would come from UI
 const poolAddress = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8';
@@ -9,10 +13,25 @@ const tokens = ['USDC', 'WETH'];
 const startTime = 1625037000;
 const endTime = 1625038000;
 
+let flows: Flow[];
 let flowIndex = 0;
+function incrementFlow() {
+    if (flowIndex < flows.length - 1) {
+        flowIndex++;
+    } else {
+        flowIndex = 0;
+    }
+}
+function decrementFlow() {
+    if (flowIndex > 0) {
+        flowIndex--;
+    } else {
+        flowIndex = flows.length - 1;
+    }
+}
 
 (async () => {
-    const flows = await getFlows(poolAddress, startTime, endTime);
+    flows = await getFlows(poolAddress, startTime, endTime);
     const startBlock = flows[0].block;
     const balancesStart = await getReserves(poolAddress, startBlock);
     const poolBalances = parsePoolBalances(flows, balancesStart);
@@ -24,14 +43,16 @@ let flowIndex = 0;
     );
 
     const previous = () => {
-        flowIndex--;
+        decrementFlow();
         refesh();
     };
 
     const next = () => {
-        flowIndex++;
+        incrementFlow();
         refesh();
     };
+
+    let interval: NodeJS.Timer;
 
     document
         .querySelector<HTMLButtonElement>('#btnPrevious')!
@@ -39,4 +60,14 @@ let flowIndex = 0;
     document
         .querySelector<HTMLButtonElement>('#btnNext')!
         .addEventListener('click', next);
+    document
+        .querySelector<HTMLButtonElement>('#btnStart')!
+        .addEventListener('click', () => {
+            interval = setInterval(next, flowDuration);
+        });
+    document
+        .querySelector<HTMLButtonElement>('#btnStop')!
+        .addEventListener('click', () => {
+            clearInterval(interval);
+        });
 })();
